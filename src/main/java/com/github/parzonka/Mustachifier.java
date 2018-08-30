ef346b036a28332b4c5a382c21e2652b76d4b8ac
xgxtprz <mateusz.parzonka@msg-gillardon.de>
2018-08-31 00:30:22 +0200

    implement fillFromYaml

--- src/main/java/com/github/parzonka/Mustachifier.java
+++ src/main/java/com/github/parzonka/Mustachifier.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.io.UncheckedIOException;
+import java.util.Map;
+
+import org.yaml.snakeyaml.Yaml;
 
 import com.github.mustachejava.DefaultMustacheFactory;
 import com.github.mustachejava.Mustache;
@@ -25,6 +28,17 @@ public class Mustachifier {
   }
 
   public String fillFromYaml(String template, String yaml) {
-    return null;
+    Yaml yamlParser = new Yaml();
+    Map<String, Object> parsed = yamlParser.load(yaml);
+    MustacheFactory mf = new DefaultMustacheFactory();
+    Mustache m = mf.compile(new StringReader(template), "");
+    StringWriter writer = new StringWriter();
+    try {
+      m.execute(writer, parsed)
+          .flush();
+      return writer.toString();
+    } catch (IOException e) {
+      throw new UncheckedIOException(e);
+    }
   }
 }
